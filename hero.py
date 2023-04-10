0246a9c7f6378b5209eec26c677626494eecd246
Veggyo <veggyscott@gmail.com>
2023-04-10 23:53:02 +0600

    'ignore'

--- hero.py
+++ hero.py
@@ -26,7 +26,54 @@ class SuperHero:
 
 people1 = SuperHero('Тор', 'Повелитель молнии','Молот и гром', 500, 'Хаймдалл! Открой радужный мост!')
 
+
+class EarthHero(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+          super().__init__(name, nickname, superpower, health_points, catchphrase)
+          self.damage = damage
+          self.fly = fly
+
+    def get_healht(self):
+          self.fly = True
+          return self.health_points ** 2
+
+    def __str__(self):
+          print('fly in the True_phrase')
+
+earth_hero = EarthHero('Ivan Goncharov', 'Haed Chamberlain', 'The precipice', 2700, "God is gracious", 1000)
+
+class AirHero(SuperHero):
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+
+    def get_healht(self):
+        self.fly = True
+        return self.health_points ** 2
+
+    def __str__(self):
+        print('fly in the True_phrase')
+
+air_hero = AirHero('Yuno', 'Meal-Saving Prince', 'Wind magic', 2550, "I'm not givving up", 1000)
+
+class Villain(AirHero):
+
+    def init(self, name, nickname, superpower, health_points, catchphrase, damage, fly=False):
+        AirHero.__init__(name, nickname, superpower, health_points, catchphrase, damage, fly)
+        SuperHero.people = 'monster'
+
+    def gen_X(self):
+        pass
+
+    def crit(self):
+        return self.damage ** 2
+
+villain = Villain('Бэтмен', 'bat', 'beast', 3500, "All heroes must to die!!!", 1500)
+
 print(people1.get_name())
 print(people1)
 print(people1.__len__())
 print(people1.get_healht())
+print(villain.crit())
